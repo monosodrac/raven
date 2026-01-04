aa2c6dac21340d8f8b73279aa4a169f37c9b9c2e
monosodrac <monosodrac@gmail.com>
2026-01-03 22:01:57 -0300

    configure cors_allowed in production

--- backend/raven/settings.py
+++ backend/raven/settings.py
@@ -75,6 +75,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://monosodrac.pythonanywhere.com",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
@@ -102,6 +103,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://monosodrac.pythonanywhere.com",
 ]
 
 # --------------------------------------------------------------
